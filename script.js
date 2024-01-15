73f8484af312a807b917cd2a2af6a786f49724cc
csongam7 <csongam7@gmail.com>
2024-01-15 15:03:18 +0100

    add operator operator function

--- script.js
+++ script.js
@@ -13,3 +13,19 @@ function multiply(number1, number2){
 function divide(number1, number2){
     return number1 / number2;
 }
+
+function operator(operator, number1, number2){
+    switch (operator){
+        case "add":
+            return add(number1, number2);
+        
+        case "subtract":
+            return subtract(number1, number2);
+        
+        case "multiply":
+            return multiply(number1, number2);
+        
+        case "divide":
+            return divide(number1, number2);
+        }
+}
