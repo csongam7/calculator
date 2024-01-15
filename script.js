09ec4ce0fedc130316698537fa9fd3e796f39658
csongam7 <csongam7@gmail.com>
2024-01-15 12:49:00 +0100

    add subtract function

--- script.js
+++ script.js
@@ -1,3 +1,7 @@
 function add(numbers){
     return (numbers.reduce((accumulator, current) => {return accumulator + current;}, 0)); 
 }
+
+function subtract(numbers){
+    return (numbers.reduce((accumulator, current) => {return accumulator - current;}, 0));
+}
