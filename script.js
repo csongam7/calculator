26da09d8ccdff2fb139a509479b873823661b1df
csongam7 <csongam7@gmail.com>
2024-01-17 13:47:55 +0100

    add working calulate function

--- script.js
+++ script.js
@@ -10,37 +10,38 @@ function setDisplayValueToDefault(){
 
 function appendDisplayValue(value){
     displayValue.push(value);
-    console.log(displayValue);
 }
 
 function add(number1, number2){
-    return number1 + number2;
+    return parseFloat(number1 + number2);
 }
 
 function subtract(number1, number2){
-    return number1 - number2;
+    return parseFloat(number1 - number2);
 }
 
 function multiply(number1, number2){
-    return number1 * number2;
+    return parseFloat(number1 * number2);
 }
 
 function divide(number1, number2){
-    return number1 / number2;
+    return parseFloat(number1 / number2);
 }
 
-function operator(operator, number1, number2){
+function operatorOfCalculations(operator, number1, number2){
+    number1 = parseFloat(number1);
+    number2 = parseFloat(number2);
     switch (operator){
-        case "add":
+        case "+":
             return add(number1, number2);
         
-        case "subtract":
+        case "-":
             return subtract(number1, number2);
         
-        case "multiply":
+        case "*":
             return multiply(number1, number2);
         
-        case "divide":
+        case "/":
             return divide(number1, number2);
         }
 }
@@ -52,6 +53,11 @@ function buttonClickHandler(button){
     displayClickedButton(button.innerText);
 }
 
+function displayResult(result){
+    const display = document.querySelector("#display");
+    display.innerText = result;
+}
+
 function displayClickedButton(character){
     const display = document.querySelector("#display");
     if (character == "Clear"){
@@ -61,8 +67,10 @@ function displayClickedButton(character){
     }
 
     else if(character == "="){
-        operator();
+        appendDisplayValue(display.innerText)
+        calculate();
     }
+
     else if (["-", "+", "*", "/"].includes(character)){
         appendDisplayValue(display.innerText);
         display.innerText += character;
@@ -77,4 +85,16 @@ function displayClickedButton(character){
         return;
     }
     display.innerText += character;
+}
+
+function calculate(){
+    const operators = [ "*", "/", "+", "-"];
+    operators.forEach( (operator) => {
+        if(displayValue.includes(operator)){
+        const num1 = displayValue[displayValue.indexOf(operator) - 1]
+        const num2 = displayValue[displayValue.indexOf(operator) + 1]
+        const result = operatorOfCalculations(operator, num1, num2) 
+        displayValue.splice(displayValue.indexOf(operator) - 1, 3, result);
+    }
+    })
 }
